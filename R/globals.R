543305349dbf24b30cbb6516c532508aac77e5ed
Philipp Junk <philipp.junk@ucdconnect.ie>
2023-04-09 23:34:06 +0100

    Added `id_mapping.R`.

--- R/globals.R
+++ R/globals.R
@@ -3,5 +3,6 @@ utils::globalVariables(c(
 	'LFQ', 'label', 'id', 'group', 'biol_repl', 'tech_repl', '.fittedPC1',
 	'.fittedPC2', 'LFQ_1', 'LFQ_2', 'count', 'n', 'new_label', 'set',
 	'present', 'best_present_ratio', 'n_group', 'present_ratio', 'present_samples',
-	'P.Value', 'contrast', 'logFC', 'logfc', 'p.value', 'pval'
+	'P.Value', 'contrast', 'logFC', 'logfc', 'p.value', 'pval',
+	'hgnc_id', 'input', 'output', 'out_id', 'symbol'
 ))
